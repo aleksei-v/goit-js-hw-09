927c37f3ee9f9d354857e36448f7e5ac0f4ccb89
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-08-05 00:35:27 +0300

    add interval with changing color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+const isChangingColor = () => refs.body.style.backgroundColor = getRandomHexColor();
 
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+
+}
+
+let intervalId = null;
+let isActive = false;
+refs.stop.setAttribute("disabled", "true")
+const startChangingColor = () => {
+    if (isActive) {
+        return
+    }
+
+    isActive = true;
+    refs.start.setAttribute("disabled", "true");
+    refs.stop.removeAttribute("disabled");
+    refs.start.style.cursor = "not-allowed";
+    intervalId = setInterval(isChangingColor, 1000);
+    
+}
+const stopChangingColor = () => { 
+    clearInterval(intervalId);
+    isActive = false;
+    refs.start.removeAttribute("disabled");
+    refs.start.style.cursor = "pointer";
+    refs.stop.setAttribute("disabled", "true");
+}
+
+refs.start.addEventListener("click", startChangingColor);
+refs.stop.addEventListener('click', stopChangingColor);
