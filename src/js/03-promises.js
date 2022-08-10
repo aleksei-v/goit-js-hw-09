c264b2d1a2d183b8fdbc26cb5710727f53f56f05
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-08-10 13:27:54 +0300

    add creator of promises (task 3)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector("form");
+
+function promisingByCicle(delay, step, amount) {
+    for (let i = 1; i <= amount; i += 1) {
+        let total = delay + step * (i - 1);
+        createPromise(i, total)
+            .then(({ position, delay }) => {
+                return Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                return Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+    }
 }
+function CreatePromiseByAmount(evt) {
+    evt.preventDefault();
+    const delay = Number(evt.target.delay.value);
+    const step = Number(evt.target.step.value);
+    const amount = Number(evt.target.amount.value);
+    promisingByCicle(delay, step, amount)
+}
+
+function createPromise(position, delay) {
+        return new Promise((resolve, reject) => {
+            const shouldResolve = Math.random() > 0.3;
+            setTimeout(() => {
+                if (shouldResolve) {
+                    resolve({ position, delay });
+                } else {
+                    reject({ position, delay });
+                }
+            }, delay)
+        });
+}
+
+form.addEventListener('submit', CreatePromiseByAmount)
+
